a9ed92fe86ecb23064e38f0b1b7693c56738244d
Aapjuh <152358287+Aapjuh@users.noreply.github.com>
2024-01-13 06:48:52 +0100

    Update RoyalRoad-Forum-Mark-Read.user.js

--- RoyalRoad-Forum-Mark-Read.user.js
+++ RoyalRoad-Forum-Mark-Read.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name			RoyalRoad Forum Mark Read
 // @namespace		RoyalRoad Forum Mark Read
-// @version			0.1
+// @version			0.2
 // @description		Marks forums categories as Read on RoyalRoad Forum.
 // @author			Aapjuh
 // @icon			https://raw.githubusercontent.com/Aapjuh/RoyalRoad-Forum-Mark-Read/main/RR-logo.png
@@ -10,27 +10,31 @@
 // @downloadURL		https://github.com/Aapjuh/RoyalRoad-Forum-Mark-Read/raw/main/RoyalRoad-Forum-Mark-Read.user.js
 // @updateURL		https://github.com/Aapjuh/RoyalRoad-Forum-Mark-Read/raw/main/RoyalRoad-Forum-Mark-Read.user.js
 // @match			https://www.royalroad.com/forums
-// @icon			https://www.google.com/s2/favicons?sz=64&domain=royalroad.com
-// @grant			GM_addStyle
-// @require			http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @grant			none
 // ==/UserScript==
 //
-'use strict';
-//--- CSS styles
-GM_addStyle(".RRMarkReadButton {position: fixed;bottom: 1em;left: 1em;z-index: 6666;}");
-GM_addStyle(".RRMarkReadButton button {color: black;background: grey;border: 1px solid #73AD21;cursor: pointer;font-weight: 700;z-index: 6666;}");
-GM_addStyle(".RRMarkReadButton:hover, .RRMarkReadButton button:hover {background-color: green;}");
+(function() {
+    'use strict';
 
-//--- Add the button.
-$("body").append (
-	'<div class="RRMarkReadButton">'
-	+ '<button id="RRMarkReadButtonBtn">Mark as Read</button></div>'
-);
+    $(document).ready(function() {
+        //--- Add the button.
+        $("body").append (
+            '<div class="RRMarkReadButton"><button id="RRMarkReadButtonBtn">Mark as Read</button></div>'
+        );
 
-//--- Activate the button click-handler.
-$("#RRMarkReadButtonBtn").click ( function () {
-	var $RRUnreadPosts = document.querySelectorAll('.forum_unread');
-	$RRUnreadPosts.forEach(node => {
-	node.click()
-	});
-});
+        //--- CSS styles
+        $(".RRMarkReadButton").css({"position": "fixed", "bottom": "1em", "left": "1em", "z-index": "6666"});
+        $("#RRMarkReadButtonBtn").css({"color": "black", "background": "grey", "border": "1px solid #73AD21", "cursor": "pointer", "font-weight": "700", "z-index": "6666"});
+        $("#RRMarkReadButtonBtn").hover(function(e) {
+            $(this).css("background-color",e.type === "mouseenter"?"green":"grey")
+        })
+
+        //--- Activate the button click-handler.
+        $("#RRMarkReadButtonBtn").click ( function () {
+            var $RRUnreadPosts = document.querySelectorAll('.forum_unread');
+            $RRUnreadPosts.forEach(node => {
+                node.click()
+            });
+        })
+    });
+})();
